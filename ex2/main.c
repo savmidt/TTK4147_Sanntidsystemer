1e2c0105ab55664832d5fb803129b308d63300f9
Såvi Midtveit <saavim@stud.ntnu.no>
2026-09-07 17:18:22 +0200

    implemented functions for excersise 2

---
+++ ex2/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "array.h"
+
+
+int main(){
+
+    
+    long xy_size    = 1000*1000*500;       // 4 GB (sizeof(long) = 8 bytes)
+    long x_dim      = 100;
+    long y_dim      = xy_size/x_dim;   
+     
+    long** matrix   = malloc(y_dim*sizeof(long*));
+
+    for(long y = 0; y < y_dim; y++){
+        matrix[y] = malloc(x_dim*sizeof(long));
+    }
+
+    printf("Allocation complete (press any key to continue...)\n");
+    getchar();
+    
+    return 0;
+}
